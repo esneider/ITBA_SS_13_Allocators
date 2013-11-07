6041fd3011ecfde1d10cbc99880dc3bc98eb348a
esneider <dariosn@gmail.com>
2013-11-07 07:57:30 -0300

    Better timing in linux too

--- simulator/timing.h
+++ simulator/timing.h
@@ -1,44 +1,62 @@
 #ifdef __APPLE__
 
-#include <CoreServices/CoreServices.h>
-#include <mach/mach.h>
-#include <mach/mach_time.h>
-#include <unistd.h>
+    #include <CoreServices/CoreServices.h>
+    #include <mach/mach.h>
+    #include <mach/mach_time.h>
+    #include <unistd.h>
 
-#define TIME_T uint64_t
+    #define TIME_T uint64_t
 
-static inline TIME_T get_time(void) {
+    static inline TIME_T get_time(void) {
 
-    return mach_absolute_time();
-}
-
-static inline double elapsed_time(TIME_T start, TIME_T end) {
-
-    static mach_timebase_info_data_t tinfo;
-
-    if (tinfo.denom == 0) {
-        (void) mach_timebase_info(&tinfo);
+        return mach_absolute_time();
     }
 
-    return ((end - start) * tinfo.numer / tinfo.denom) / 1000.0;
-}
+    static inline double elapsed_time(TIME_T start, TIME_T end) {
 
-#else /* __APPLE__ */
+        static mach_timebase_info_data_t tinfo;
 
-#include <sys/time.h>
+        if (tinfo.denom == 0) {
+            (void) mach_timebase_info(&tinfo);
+        }
 
-#define TIME_T struct timeval
+        return ((end - start) * tinfo.numer / tinfo.denom) / 1000.0;
+    }
 
-static inline TIME_T get_time(void) {
+#elif __linux
 
-    TIME_T ret;
-    gettimeofday(&ret, NULL);
-    return ret;
-}
+    #include <time.h>
 
-static inline double elapsed_time(TIME_T start, TIME_T end) {
+    #define TIME_T struct timespec
 
-    return end.tv_usec - start.tv_usec;
-}
+    static inline TIME_T get_time(void) {
 
-#endif /* __APPLE__ */
+        TIME_T ret;
+        clock_gettime(CLOCK_MONOTONIC, &ret);
+        return ret;
+    }
+
+    static inline double elapsed_time(TIME_T start, TIME_T end) {
+
+        return (end.tv_nsec - start.tv_nsec) / 1000.0;
+    }
+
+#else /* !__APPLE__ && !__linux */
+
+    #include <sys/time.h>
+
+    #define TIME_T struct timeval
+
+    static inline TIME_T get_time(void) {
+
+        TIME_T ret;
+        gettimeofday(&ret, NULL);
+        return ret;
+    }
+
+    static inline double elapsed_time(TIME_T start, TIME_T end) {
+
+        return end.tv_usec - start.tv_usec;
+    }
+
+#endif
