0488541b5ea689eeffbee0c408030e9222abfcef
axel <w@lap-ubuntu.(none)>
2013-12-05 01:21:44 -0300

    markov graphs

--- generate_samples.bash
+++
@@ -1,6 +0,0 @@
-./simulator/run -c context/samples/excel.txt -h 100000 -t 6000 -o samples/excel.txt
-./simulator/run -c context/samples/iTunes.txt -h 100000 -t 6000 -o samples/iTunes.txt
-./simulator/run -c context/samples/photoshop.txt -h 100000 -t 6000 -o samples/photoshop.txt
-./simulator/run -c context/samples/safari.txt -h 100000 -t 6000 -o samples/safari.txt
-./simulator/run -c context/samples/skype.txt -h 100000 -t 6000 -o samples/skype.txt
-./simulator/run -c context/samples/vim.txt -h 100000 -t 6000 -o samples/vim.txt
---
+++ generate_samples.sh
@@ -0,0 +1,11 @@
+./bin/simulator/run_best -c context/samples/excel.txt -h 100000000 -t 3000 -o samples/excel_best.txt
+./bin/simulator/run_best -c context/samples/photoshop.txt -h 100000000 -t 3000 -o samples/photoshop_best.txt
+./bin/simulator/run_best -c context/samples/safari.txt -h 100000000 -t 3000 -o samples/safari_best.txt
+./bin/simulator/run_best -c context/samples/skype.txt -h 100000000 -t 3000 -o samples/skype_best.txt
+./bin/simulator/run_best -c context/samples/vim.txt -h 100000000 -t 3000 -o samples/vim_best.txt
+
+./bin/simulator/run_lifo -c context/samples/excel.txt -h 100000000 -t 3000 -o samples/excel_lifo.txt
+./bin/simulator/run_lifo -c context/samples/photoshop.txt -h 100000000 -t 3000 -o samples/photoshop_lifo.txt
+./bin/simulator/run_lifo -c context/samples/safari.txt -h 100000000 -t 3000 -o samples/safari_lifo.txt
+./bin/simulator/run_lifo -c context/samples/skype.txt -h 100000000 -t 3000 -o samples/skype_lifo.txt
+./bin/simulator/run_lifo -c context/samples/vim.txt -h 100000000 -t 3000 -o samples/vim_lifo.txt
